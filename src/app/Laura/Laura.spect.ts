c8f8c85adb5dfab89e602c91f927b04a5d6d6a3f
laura-isabel-cohetero-vargas <lau_coh@hotmail.com>
2019-03-14 03:04:46 -0600

    operaracion de porcentaje

--- src/app/Laura/Laura.spect.ts
+++ src/app/Laura/Laura.spect.ts
@@ -1,9 +1,21 @@
 import { getOperacion } from './getOperacion';
+import { getOperacion2 } from './getOperacion';
+
 
 describe ('getOperacion', () => {
     it ('Should return 125 if input is 5*5*5', () => {
         const result = getOperacion(5);
         expect(result).toBe(125);
+       
+    } );
+});
+
+
+
+describe ('getOperacion2', () => {
+    it ('Should return 16 if input is 100', () => {
+       const result = getOperacion2(10);
+        expect(result).toBe(16);
        
     } );
 });
