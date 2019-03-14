a466f0a1de8a1d36ee6a92057218b2cdf6201d64
ArielSaurio <amc_mer11@hotmail.com>
2019-03-14 03:59:06 -0600

    Multi 3

---
+++ src/Ariel /mul3num.spec.ts
@@ -0,0 +1,11 @@
+import {res3num} from './res3num';
+import {} from 'jasmine' ;
+describe('res3num',()=>{
+    
+it ('resta de 3 numeros',()=>{
+  let mydiv = res3num(10,2,2);
+    let result = res3num(10,2,2);
+    expect(result).toBe(6);
+    
+    })
+})
