97859c7530d4d0633efa1b11c4bc84730104664e
leticia rafael <lety02>
2019-03-14 12:37:19 -0600

    operacion resta

--- src/app/Leticia/Leticia.spec.ts
+++ src/app/Leticia/Leticia.spec.ts
@@ -1,10 +1,21 @@
 import {leticia} from './Leticia';
-
+import {leticia2} from './Leticia';
 
 describe ('leticia', () => {
-    it ('Should return 125 if input is 5+5', () => {
+    it ('Should return 10 if input is 5+5', () => {
         const result = leticia(5);
-        expect(result).toBe(125);
+        expect(result).toBe(10);
+       
+    } );
+});
+
+
+
+
+describe ('leticia2', () => {
+    it ('Should return 5 if input is 10-5', () => {
+        const result = leticia(5);
+        expect(result).toBe(5);
        
     } );
 });
