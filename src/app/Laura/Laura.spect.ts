696f69e095a8f357bd408f3bf65b463f289757b9
laura-isabel-cohetero-vargas <lau_coh@hotmail.com>
2019-03-14 02:42:11 -0600

    Funcion Numero Elevado al cubo

--- src/app/Laura/Laura.spect.ts
+++ src/app/Laura/Laura.spect.ts
@@ -2,7 +2,7 @@ import { getOperacion } from './getOperacion';
 
 describe ('getOperacion', () => {
     it ('Should return 125 if input is 5*5*5', () => {
-        const result = getOperacion(5,5,5);
+        const result = getOperacion(5);
         expect(result).toBe(125);
        
     } );
---
+++ src/app/Laura/getOperacion.ts
@@ -0,0 +1,4 @@
+
+export function getOperacion(numero){
+    return numero
+}
