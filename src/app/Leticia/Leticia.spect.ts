1bd1fed3ed20f17324b59410a15f7153fe687f47
leticia rafael <lety02>
2019-03-14 10:15:33 -0600

    prueba resta

--- src/app/Leticia/Leticia.spect.ts
+++ src/app/Leticia/Leticia.spect.ts
@@ -1,6 +1,17 @@
-import {leticia} from './Leticia'
+import {leticia,leticia2} from './Leticia'
 
 describe ('leticia',()=> {
+    const result=leticia;
+    expect (result).toBe(25);
+})
+
+
+
+
+
+
+
+describe ('leticia2',()=> {
     const result=leticia;
     expect (result).toBe(25);
 })
